659f68a1c43b462f91647ed6178d1160367776dd
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-11-18 16:11:46 +0100

    Create PrettyPrint.java

---
+++ src/binaryTree/PrettyPrint.java
@@ -0,0 +1,26 @@
+package binaryTree;
+
+import java.util.Optional;
+
+public class PrettyPrint {
+
+    private final StringBuilder out = new StringBuilder();
+
+    public void print(TreeNode node) {
+        print(node, true);
+    }
+    public void print(TreeNode node, boolean isLeft) {
+        if (Optional.ofNullable(node).isEmpty()) {
+            out.append("Empty tree");
+        } else if (Optional.ofNullable(node.right).isPresent()) {
+            out.append(isLeft ? "│   " : "    ");
+            print(node.right, false);
+            out.append(isLeft ? "└── " : "┌── ")
+               .append(node.value);
+        } else if (Optional.ofNullable(node.left).isPresent()) {
+            out.append(isLeft ? "    " : "│   ");
+            print(node.left,true);
+        }
+        System.out.println(out);
+    }
+}
