16d4da96bbec93df194e96ef1fd1778590c95dac
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-11-18 16:11:49 +0100

    Create InputString.java

---
+++ src/linkedList/InputString.java
@@ -0,0 +1,34 @@
+package linkedList;
+
+public class InputString {
+
+    public ListNode toListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = toIntegerArray(input);
+
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for(int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+
+    private int[] toIntegerArray(String input) {
+        input = input.trim().substring(1, input.length() - 1);
+
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+}
